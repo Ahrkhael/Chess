efceaea6f9aa8eddd01de7a6a788ed5b571f01e5
Ahrkhael <jjkako@gmail.com>
2024-06-30 18:22:11 +0200

    Peones negros añadidos

---
+++ chess.js
@@ -0,0 +1,64 @@
+const tablero = document.getElementById("board")
+
+for(let i=0; i<8; i++) {
+
+    const linea = document.createElement("div")
+    linea.classList.add("linea")
+    tablero.appendChild(linea)
+
+    for (let j=0; j<8; j++) {
+
+        const casilla = document.createElement("div")
+        casilla.classList.add("casilla")
+
+        if ((i+j)%2===0) {
+            casilla.style.backgroundColor="#FFF8DC"
+        }else {
+            casilla.style.backgroundColor="#DEB887"
+        }
+
+        linea.appendChild(casilla)
+
+        if(j===7) {
+            const br = document.createElement("br")
+            linea.appendChild(br)
+        }
+    }
+}
+
+class Pieza {
+    constructor(nombre) {
+        this.nombre=nombre
+        switch (nombre) {
+            case "peon blanco":
+                this.figura="\u{2659}"
+                break
+            case "peon negro":
+                this.figura="\u{265F}"
+                break
+        }
+    }
+}
+
+const rellenarTableroInicial = () => {
+    
+    for(let i=0; i<8; i++) {
+        
+        if (i===1) {
+
+            for (let j=0; j<8; j++) {
+                
+                const casilla = document.getElementsByClassName("casilla")[j+8]
+                const pieza = document.createElement("p")
+                casilla.appendChild(pieza)
+
+                pieza.classList.add("piezas")
+
+                const peon = new Pieza("peon negro")
+                pieza.innerHTML = peon.figura
+            }
+        }
+    }
+}
+
+rellenarTableroInicial()
